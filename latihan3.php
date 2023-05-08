cc994624d7139096644281660b4a7f3b733dfab5
projectfinal3020 <132859107+projectfinal3020@users.noreply.github.com>
2023-05-08 08:02:30 +0700

    Add files via upload

---
+++ latihan3.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Percobaan PHP</title> 
+</head> 
+<body> 
+
+<?php 
+printf("Hello Word");  
+// This is a single-line comment
+# This is also a single-line comment
+?> 
+
+</body> 
+</html> 
---
+++ latihan4.php
@@ -0,0 +1,11 @@
+<?php 
+
+echo "<h2>Belajar PHP itu mudah!</h2>"; 
+echo("Hello world!<br>"); 
+echo "Aku sedang belajar PHP!<br>"; 
+echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah."; 
+
+/*This is a multiple-lines comment block that spans over multiple lines*/
+
+
+?> 
